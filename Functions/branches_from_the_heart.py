0fd639244d401aeed1ecf092cc2d1e2d88ab8efa
eachingateb <eachingateb@unal.edu.co>
2025-09-15 17:14:47 -0500

    using the shortest path to list all nodes on each branch

--- Functions/branches_from_the_heart.py
+++ Functions/branches_from_the_heart.py
@@ -1,24 +1,10 @@
 import numpy as np
-#from neighbour_in_branch import *
+import networkx as nx
 def branches_from_the_heart(TreeSeed):
-    DegreeArray=np.array(TreeSeed.degree,dtype='int')
-    LeavesLoc=np.where(DegreeArray[:,1]==1)[0]
-    LeavesList=DegreeArray[LeavesLoc,0]
+    betweenness_centrality=nx.betweenness_centrality(TreeSeed)
     top_nodes=np.array(sorted(betweenness_centrality, key=betweenness_centrality.get, reverse=True)[:5])
-    # I can also strength the function to consider several candidates to heart
-    #Top_betweenness_centrality=np.array([betweenness_centrality[key] for key in top_nodes])
-    #Top_betweenness_centrality=Max_betweenness_centrality
-    #Max_betweenness_centrality=max(Top_betweenness_centrality)
-    #HeartLoc=np.where(Top_betweenness_centrality==Max_betweenness_centrality)[0]
-    #Heart=top_nodes[int(Max_betweenness_centrality_loc[0])]
-    Heart=top_nodes[0]    
-    EdgesArray=np.array(TreeSeed.edges)
-    Branches=[]
-    for leave in LeavesList:
-        BranchList=[int(leave)]
-        BranchList=neighbour_in_branch(BranchList=BranchList,
-                                       EdgesArray=EdgesArray,
-                                       Heart=Heart)
-        Branches.append(BranchList)
-    Branches_and_Heart=[Branches,Heart]
+    Heart=int(top_nodes[0])
+    leaves = [n for n in TreeSeed.nodes() if TreeSeed.degree(n) == 1]
+    Branches= [nx.shortest_path(TreeSeed, source=leaf, target=Heart) for leaf in leaves]
+    Branches_and_Heart=[Branches,Heart]    
     return Branches_and_Heart
